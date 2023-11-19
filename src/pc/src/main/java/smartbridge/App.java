23dc2788fe64f6ae44b6fb7760061fd3a8018021
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2023-11-19 20:26:59 +0100

    Configured Java project

---
+++ src/pc/src/main/java/smartbridge/App.java
@@ -0,0 +1,19 @@
+package smartbridge;
+
+import javafx.application.Application;
+
+/**
+ * The startup class of this project.
+ */
+public final class App {
+
+    private App() { }
+
+    /**
+     * Application entry-point.
+     * @param args arguments
+     */
+    public static void main(final String[] args) {
+        Application.launch(Gui.class, args);
+    }
+}
---
+++ src/pc/src/main/java/smartbridge/Gui.java
@@ -0,0 +1,27 @@
+package smartbridge;
+
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * View main class.
+ */
+public final class Gui extends Application {
+
+    private static final double TITLE_SIZE = 100;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void start(final Stage stage) throws Exception {
+        final Label message = new Label("Hello World!"); 
+        message.setFont(new Font(TITLE_SIZE));
+        stage.setScene(new Scene(message));
+        stage.setTitle("SmartCarWashingSystem");
+        stage.show();
+    }
+}
