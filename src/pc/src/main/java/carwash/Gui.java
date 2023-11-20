78345edadf21ed4d2446f1d535f8d935fcb925a9
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-11-21 00:54:18 +0100

    Added gui fxml and fxml controller

--- src/pc/src/main/java/carwash/Gui.java
+++ src/pc/src/main/java/carwash/Gui.java
@@ -1,8 +1,8 @@
 package carwash;
 
 import javafx.application.Application;
-import javafx.scene.control.Label;
-import javafx.scene.text.Font;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,17 +11,19 @@ import javafx.stage.Stage;
  */
 public final class Gui extends Application {
 
-    private static final double TITLE_SIZE = 100;
+    private static final double MIN_HEIGHT = 450;
+    private static final double MIN_WIDTH = 600;
 
     /**
      * {@inheritDoc}
      */
     @Override
     public void start(final Stage stage) throws Exception {
-        final Label message = new Label("Hello World!"); 
-        message.setFont(new Font(TITLE_SIZE));
-        stage.setScene(new Scene(message));
-        stage.setTitle("SmartCarWashingSystem");
+        final Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/carwash.fxml"));
+        stage.setScene(new Scene(root));
+        stage.setMinHeight(MIN_HEIGHT);
+        stage.setMinWidth(MIN_WIDTH);
+        stage.setTitle("Smart Car Wash");
         stage.show();
     }
 }
