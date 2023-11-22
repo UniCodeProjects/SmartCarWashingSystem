86abc1ebeca7ebf76219e64851e0a91ebeb89f97
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-11-22 19:34:39 +0100

    Attached view controller to ViewUpdater in gui

--- src/pc/src/main/java/carwash/Gui.java
+++ src/pc/src/main/java/carwash/Gui.java
@@ -1,5 +1,7 @@
 package carwash;
 
+import carwash.controllers.ViewController;
+import carwash.controllers.ViewUpdater;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -19,7 +21,10 @@ public final class Gui extends Application {
      */
     @Override
     public void start(final Stage stage) throws Exception {
-        final Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/carwash.fxml"));
+        final FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource("layouts/carwash.fxml"));
+        final Parent root = loader.load();
+        final ViewController controller = loader.getController();
+        ViewUpdater.getInstance().attachController(controller);
         stage.setScene(new Scene(root));
         stage.setMinHeight(MIN_HEIGHT);
         stage.setMinWidth(MIN_WIDTH);
