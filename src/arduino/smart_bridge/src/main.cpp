589c9f225422af82da24e5d62b8bf1351c0b7f75
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-11-23 11:16:08 +0100

    wip: dynamic port detection

--- src/arduino/smart_bridge/src/main.cpp
+++ src/arduino/smart_bridge/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
 
+bool isHandshakeEnded = false;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // while (!isHandshakeEnded) {
+  //   String received = Serial.readStringUntil('\r');
+  //   if (received.equals("HELLO ARDUINO")) {
+  //     Serial.println("HELLO JAVA");
+  //     isHandshakeEnded = true;
+  //   }
+  // }
+  
+
+  Serial.println("HELLO JAVA");
 }
--- src/pc/src/main/java/carwash/App.java
+++ src/pc/src/main/java/carwash/App.java
@@ -1,11 +1,21 @@
 package carwash;
 
+import carwash.controllers.ViewUpdater;
+import carwash.serial.SerialChannel;
 import javafx.application.Application;
+import javafx.scene.paint.Color;
+import jssc.SerialPort;
+import jssc.SerialPortException;
+import jssc.SerialPortList;
+
+import java.util.Objects;
 
 /**
  * The startup class of this project.
  */
 public final class App {
+
+    private static volatile boolean isProgramRunning = true;
 
     private App() { }
 
@@ -14,6 +24,41 @@ public final class App {
      * @param args arguments
      */
     public static void main(final String[] args) {
+        String port = "";
+        for (final String currentPortName : SerialPortList.getPortNames()) {
+            System.out.println(currentPortName);
+            SerialPort testPort = new SerialPort(currentPortName);
+            try {
+                if (testPort.isOpened()) {
+                    continue;
+                }
+                testPort.openPort();
+                String received = testPort.readString();
+                if (Objects.nonNull(received) && received.contains("HELLO JAVA")) {
+                    port = currentPortName;
+                    testPort.closePort();
+                }
+            } catch (SerialPortException e) {
+
+            }
+        }
+        System.out.println(port);
+        final SerialChannel channel = new SerialChannel(Objects.requireNonNull(port), SerialPort.BAUDRATE_9600);
+        final Thread serialChannelThread = new Thread(() -> {
+            while (isProgramRunning) {
+                final String s = channel.receive().orElse("");
+                ViewUpdater.getInstance().getController().updateLogWindow(s);
+                ViewUpdater.getInstance().getController().updateStatusCircleColour(Color.GREEN);
+                // CHECKSTYLE: MagicNumber OFF
+                ViewUpdater.getInstance().getController().updateNumWashes(12);
+                ViewUpdater.getInstance().getController().updateTemperature(70);
+                // CHECKSTYLE: MagicNumber ON
+            }
+        });
+        serialChannelThread.start();
         Application.launch(Gui.class, args);
+        channel.closePort();
+        isProgramRunning = false;
+        serialChannelThread.interrupt();
     }
 }
