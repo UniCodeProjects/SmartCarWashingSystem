42facc5d73324780dbd18a5a2116d0b0a81ca314
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-11-24 19:25:18 +0100

    Fixed port finder script when installing the missing library

--- src/pc/src/main/resources/port_finder.py
+++ src/pc/src/main/resources/port_finder.py
@@ -1,19 +1,20 @@
 import sys
+import os
 import subprocess
 import re
 
-# Runs pip show pyserial to ensure the sys path includes the pyserial installation location
+# Runs pip show pyserial to ensure the sys path includes the pyserial installation location.
 lib_name = "pyserial"
-lib_location = subprocess.run(["pip", "show", lib_name], stdout=subprocess.PIPE, text=True).stdout
-if "WARNING: Package(s) not found" in lib_location:
-    result = subprocess.run(["pip", "install", lib_name])
-    if result.returncode != 0:
-        raise RuntimeError
-if lib_location not in sys.path:
-    match = re.search(r'^Location: (.+)$', lib_location, re.MULTILINE)
+lib_location = subprocess.run(["pip", "show", lib_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+# Installing missing library, then restarting the script.
+if lib_location.returncode != 0:    
+    result = subprocess.run(["pip", "install", lib_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    os.execl(sys.executable, sys.executable, *sys.argv)
+if lib_location.stdout.strip() not in sys.path:
+    match = re.search(r'^Location: (.+)$', lib_location.stdout, re.MULTILINE)
     if match:
         lib_location = match.group(1)
-    sys.path.append(lib_location)
+        sys.path.append(lib_location)
 
 import serial.tools.list_ports
 
