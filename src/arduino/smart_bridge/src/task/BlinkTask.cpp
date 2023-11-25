63928d6e68820f061cbe18021aa80b4804b05a94
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2023-11-25 18:39:02 +0100

    Implemented enabling and disabling feature for BlinkTask

--- src/arduino/smart_bridge/src/task/BlinkTask.cpp
+++ src/arduino/smart_bridge/src/task/BlinkTask.cpp
@@ -2,7 +2,27 @@
 
 BlinkTask::BlinkTask(Led* const led, const int period) : TaskImpl(period) {
     this->led = led;
+    isBlinkEnabled = false;
     state = OFF;
+}
+
+bool BlinkTask::canBlink() {
+    return this->isBlinkEnabled;
+}
+
+void BlinkTask::enableBlink() {
+    this->isBlinkEnabled = true;
+}
+
+void BlinkTask::disableBlink() {
+    this->isBlinkEnabled = false;
+}
+
+bool BlinkTask::canStart(const int schedulerPeriod) {
+    /* if the blink is enabled, than the method canStart() of the superclass is called. In this way,
+     * the value of the field timeElapsed is not updated when the blink is not enabled, avoiding problems.
+     */
+    return isBlinkEnabled ? TaskImpl::canStart(schedulerPeriod) : false;
 }
 
 void BlinkTask::start() {
