bf62760f104fc3bc99b37cfa26d0132ff3dd75a4
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-11-28 10:46:41 +0100

    Added doc for button task

--- src/arduino/smart_bridge/src/task/ButtonTask.cpp
+++ src/arduino/smart_bridge/src/task/ButtonTask.cpp
@@ -1,5 +1,6 @@
 #include "task/ButtonTask.h"
 
+/// @brief The global variable to determine whether the button is pressed or not.
 bool isBtnPressed;
 
 ButtonTask::ButtonTask(Button* const button, const int period) : TaskImpl(period) {
