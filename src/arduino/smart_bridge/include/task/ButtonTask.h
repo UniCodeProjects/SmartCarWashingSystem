135eab5138e95e558bb2678c8090668ca937e7e1
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-11-28 10:46:41 +0100

    Added doc for button task

--- src/arduino/smart_bridge/include/task/ButtonTask.h
+++ src/arduino/smart_bridge/include/task/ButtonTask.h
@@ -5,9 +5,15 @@
 #include "components/Button.h"
 #include "components/ButtonImpl.h"
 
+/// @brief The button task.
 class ButtonTask : public TaskImpl {
     public:
+     /// @brief The button task constructor.
+     /// @param button the button linked to this task
+     /// @param period the task period in ms
      ButtonTask(Button* const button, const int period);
+
+     /// @brief Starts the task and set the isBtnPressed global variable.
      void start();
 
     private:
