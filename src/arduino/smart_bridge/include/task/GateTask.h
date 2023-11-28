1437bc44af4d318904bb53f6d725669248f19e26
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-11-28 10:29:10 +0100

    Added doc for gate task

--- src/arduino/smart_bridge/include/task/GateTask.h
+++ src/arduino/smart_bridge/include/task/GateTask.h
@@ -5,9 +5,15 @@
 #include "components/ServoMotor.h"
 #include "components/ServoMotorImpl.h"
 
+/// @brief The gate task that operates the opening and closing of the servo motor.
 class GateTask : public TaskImpl {
     public:
+     /// @brief The gate task constructor.
+     /// @param motor the servo motor that acts as a gate
+     /// @param period the task period in ms
      GateTask(ServoMotor* const motor, const int period);
+
+     /// @brief Starts the task.
      void start();
     
     private:
